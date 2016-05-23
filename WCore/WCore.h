a8d4fe19d123152d8859885e4b141722689ea3e8
aramikg <aramik@whalerockindustries.com>
2016-05-22 17:06:17 -0700

    Initial Commit

---
+++ WCore/WCore.h
@@ -0,0 +1,19 @@
+//
+//  WCore.h
+//  WCore
+//
+//  Created by aramik on 5/22/16.
+//  Copyright © 2016 Whalerock Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WCore.
+FOUNDATION_EXPORT double WCoreVersionNumber;
+
+//! Project version string for WCore.
+FOUNDATION_EXPORT const unsigned char WCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WCore/PublicHeader.h>
+
+
