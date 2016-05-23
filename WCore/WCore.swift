656e92793947eba7cfe00be725416a04c6d76627
aramikg <aramik@whalerockindustries.com>
2016-05-22 19:44:17 -0700

    updated project

--- WCore/WCore.swift
+++ WCore/WCore.swift
@@ -1,6 +1,6 @@
 //
 //  WCore.swift
-//  WCore
+//  WCoreProject
 //
 //  Created by aramik on 5/22/16.
 //  Copyright © 2016 Whalerock Industries. All rights reserved.
@@ -11,7 +11,7 @@ import Foundation
 public class WCore {
     public static let sharedInstance = WCore()
     
-    public var deviceUUID: String? {
-        return UIDevice.currentDevice().identifierForVendor?.UUIDString
+    public func test() {
+        print("welcome to wcore!")
     }
 }
